aa4497d145220ea15fe200caa00a9c27f0bce23d
Benjamin Suire <bensuire@gmail.com>
2014-11-23 00:55:23 -0500

    basic chat app created

---
+++ app.js
@@ -0,0 +1,27 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+// listen on connection event for new users
+io.on('connection', function(socket){
+    console.log('new user connected');
+
+    // message received
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg); // log message into server
+        io.emit('chat message', msg);
+    });
+
+    // listen on disconnection events for sockets for users leaving
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});
+
+http.listen(3000, function(){
+      console.log('listening on *:3000');
+});
